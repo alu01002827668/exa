9d8d142bf4b117e457d24dbb33e63ddf72946594
Yessica Gomez <alu0100827668@ull.edu.es>
2014-09-02 21:51:13 +0100

    modulo

---
+++ src/solucion.py
@@ -0,0 +1,14 @@
+#!/src/bin/python
+
+#solucion de la aproximacion 
+
+import modulo, math 
+
+lista_entrada = [1,2,3,4,5]
+lista_resultado = []
+
+for i in lista_resultado:
+  x = modulo.factorial(i)
+  lista_resultado = lista_resultado + [x]
+  print [x]
+  
