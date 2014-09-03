c2d5c4f91b3f4f4fb7e63fc62c93bb308d56ff57
Yessica Gomez <alu0100827668@ull.edu.es>
2014-09-03 18:15:19 +0100

    tiempo

--- src/solucion.py
+++ src/solucion.py
@@ -3,12 +3,22 @@
 #solucion de la aproximacion 
 
 import modulo, math 
+import time, timeit
+import matplotlib.pylab as pl
 
+lista_tiempo = []
 lista_entrada = [1,2,3,4,5]
 lista_resultado = []
 
-for i in lista_resultado:
+for i in lista_entrada:
+  start=time.time()
   x = modulo.factorial(i)
   lista_resultado = lista_resultado + [x]
-  print [x]
+  finish=time.time()-start
+  print 'La aproximacion de ',i,'es  ', x
+  print 'El tiempo que se tardo ha sido %14.13f' %finish
+  lista_tiempo = lista_tiempo + [finish]
+
+print lista_resultado
+
   
